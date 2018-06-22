277f1022d2fc53a0aecad32bcaf89b729a778e50
ArtemeeSenin <artemee.senin@gmail.com>
2018-06-22 21:24:56 +0300

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import logo from './logo.png';
 import './App.css';
-
+import Studio from './components/Studio';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <Router>
+      <div className="container">
+        <header className="row">
+          <div className="logo col-2 offset-1">
+            <img src={logo} alt="hair studio logotype" />
+          </div>
+          <nav className="col-8 header-nav">
+              <ul className="header-nav__container">
+                <li className="header-nav__item"><Link to="/">Home</Link></li>
+              </ul>
+          </nav>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>          
+          <Route exact path="/" component={Studio} />
+        </main>
+        <footer className="row page-footer mt-1">
+          <div className="col-10 offset-1">
+            <small>&copy; Hair Studio 2018</small>
+          </div>
+        </footer>
       </div>
+      </Router>
     );
   }
 }
